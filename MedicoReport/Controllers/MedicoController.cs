688e738bb6b82344d4c078da2908e72d65225cb6
alanlb195 <alanlopezbarrientos@live.com.mx>
2023-07-24 23:29:47 -0500

    Proyecto base, reporte en blanco

---
+++ MedicoReport/Controllers/MedicoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MedicoReport.Controllers
+{
+    public class MedicoController : Controller
+    {
+        // GET: Medicos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
